f85c8e3bbb950f01980122ed3cabd080450baa33
ukralpeps <70707611+ukralpeps@users.noreply.github.com>
2023-02-08 20:00:50 +0300

    Add files via upload

---
+++ lab1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace lab1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<Action<float, float>, bool> func = GetTrue; //создание делегата 
+            Action<float, float> action = Draw; //инициализация делегата
+            func.Invoke(action); //вызов делегата
+            func = GetFalse; // инициализация делегата
+            func.Invoke(action); //вызов делегата
+
+        }
+
+        static void Draw(float x, float y) //метод вывода на экран
+        {
+            Console.WriteLine($"{x} and {y}");
+
+        }
+        static bool GetTrue(Action<float, float> draw) //true
+        {
+            draw.Invoke(1.33f, 8.17f);
+            return true;
+        }
+        static bool GetFalse(Action<float, float> draw) //false
+        {
+            draw.Invoke(0, 35.177f);
+            return false;
+        }
+
+
+    }
+}
